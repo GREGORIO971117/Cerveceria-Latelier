2bfb2142ae3e2f5936ccd3f855fe45344b859f6b
Gregorio <gregorio.galindo97@gmail.com>
2025-05-20 23:57:24 -0600

    Se suben cambios de afiliados

--- js/js_afiliados/afiliados-data.js
+++ js/js_afiliados/afiliados-data.js
@@ -1,80 +1,30 @@
-export const items= [{
+export const items = [
+  {
     'name': 'Corona media',
-    'id':'1',
-    'img': '../../assets/img-afiliados/Corona_1.jpeg',
+    'id': '1',
+    'img': 'assets/img-afiliados/Corona_1.jpeg',
     'descripción': 'Corona media',
     'rating': '5.0',
-    'detalles': 'Es un spa y salón de belleza que ofrece una experiencia única de relajación y cuidado personal. Con un equipo altamente capacitado brindando y cuidando siempre la calidad de sus servicios. Ofrecen una amplia gama de tratamientos faciales y corporales, así como servicios de peluquería y estética. Su ambiente acogedor y profesional garantiza una experiencia inolvidable para cada cliente.',
-    "servicios": {
-        '0': 'SPA',
-        '1': 'Corte',
-        '2': 'Masaje',
-    },
-    "imagenServicios":{
-        '0': '../../assets/img-servicios/spa.jpg',
-        '1': '../../assets/img-servicios/corte.jpg',
-        '2': '../../assets/img-servicios/masaje.jpg',
-        
-    }
-},
-
-{
+  },
+  {
     'name': 'Victoria latón',
-    'id':'2',
-    'img': '../../assets/img-afiliados/Victoria.jpeg',
+    'id': '2',
+    'img': 'assets/img-afiliados/Victoria.jpeg',
     'descripción': 'Victoria latón',
     'rating': '4.5',
-    'detalles': 'Es un spa que se especializa en tratamientos de belleza y bienestar. Ofrecen servicios con una calidad excepcional y un enfoque personalizado para cada cliente. Además de contar con productos de alta gamma y personal altamente capacitado para consentirte.',
-    "servicios": {
-        '0': 'Barbería',
-        '1': 'Corte',
-        '2': 'Facial',
-    },
-    "imagenServicios":{
-        '0': '../../assets/img-servicios/barberia.jpg',
-        '1': '../../assets/img-servicios/corte.jpg',
-        '2': '../../assets/img-servicios/faciales.jpg',
-        
-    }
-},
-
-{
+  },
+  {
     'name': 'Dos XX lata',
-    'id':'3',
-    'img': '../../assets/img-afiliados/DosXX_1.jpeg',
+    'id': '3',
+    'img': 'assets/img-afiliados/DosXX_1.jpeg',
     'descripción': 'DosXX',
     'rating': '5.0',
-    'detalles': 'Barbería especializada en cortes de cabello y afeitados. Ofrecen una experiencia única de relajación y servicio personalizado. Cuentan con certificaciones y un equipo que buscara siempre la calidad en sus servicios, con un estilo fresco y moderno.',
-    "servicios": {
-        '0': 'SPA',
-        '1': 'Corte',
-        '2': 'Masaje',
-    },
-    "imagenServicios":{
-        '0': '../../assets/img-servicios/spa.jpg',
-        '1': '../../assets/img-servicios/corte.jpg',
-        '2': '../../assets/img-servicios/masaje.jpg',
-        
-    }
-},
-
-{
+  },
+  {
     'name': 'Dos XX ampolleta ',
-    'id':'4',
-    'img': '../../assets/img-afiliados/DosXX_2.jpeg',
+    'id': '4',
+    'img': 'assets/img-afiliados/DosXX_2.jpeg',
     'descripción': 'DosXX ampolleta',
     'rating': '4.5',
-    'detalles': 'Es un estudio de uñas que diseña y crea uñas acrílicas, gelish y decoradas. Ofrecen una amplia gama de servicios de manicura y pedicura, con un enfoque en la calidad y la atención al detalle. Su equipo de profesionales está altamente capacitado para brindar una experiencia única y personalizada a cada cliente.',
-    "servicios": {
-        '0': 'Manicure',
-        '1': 'Pedicure',
-        '2': 'Gelish',
-    },
-    "imagenServicios":{
-        '0': '../../assets/img-servicios/manicure.jpg',
-        '1': '../../assets/img-servicios/pedicure.jpg',
-        '2': '../../assets/img-servicios/gelish.jpg',
-        
-    }
-}
+  }
 ]
