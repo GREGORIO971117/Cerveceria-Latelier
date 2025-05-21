95927fc6a0401f56102290b912e16c0657e73045
Gregorio <gregorio.galindo97@gmail.com>
2025-05-20 23:43:00 -0600

    Se actualizan rutas de fotos nosotros

--- js/js_nosotros/nosotros_data.js
+++ js/js_nosotros/nosotros_data.js
@@ -1,25 +1,25 @@
 export const items= [{
     'name': 'Monarca Coyotl Christian Angelo',
     'id':'1',
-    'img': '../../assets/img-nosotros/Monarca.jpeg',
+    'img': './assets/img-nosotros/Monarca.jpeg',
     'puesto':'Director General',
 },
 {
     'name': 'Lopez Espinoza Oscar',
     'id':'2',
-    'img': '../../assets/img-nosotros/Oscar.jpeg',
+    'img': './assets/img-nosotros/Oscar.jpeg',
     'puesto':'Marketing',
 },
 {
     'name': 'Mariana Galindo Tehuitzil',
     'id':'3',
-    'img': '../../assets/img-nosotros/Mariana.jpg',
+    'img': './assets/img-nosotros/Mariana.jpg',
     'puesto':'Finanzas',
 },
 {
     'name': 'De Ita Lector Evelyn',
     'id':'4',
-    'img': '../../assets/img-nosotros/Evelin.jpeg',
+    'img': './assets/img-nosotros/Evelin.jpeg',
     'puesto':'Gerente general',
     "redes": {
         '0': 'https://github.com/adan2990',
@@ -29,13 +29,13 @@ export const items= [{
 {
     'name':'Leonor Tlahuel Luis Karol',
     'id':'5',
-    'img': '../../assets/img-nosotros/Luis.jpeg',
+    'img': './assets/img-nosotros/Luis.jpeg',
     'puesto':'Compras'
 },
 {
     'name': 'Isela Belem De La Mora Angel',
     'id':'6',
-    'img': '../../assets/img-nosotros/Isela.jpeg',
+    'img': './assets/img-nosotros/Isela.jpeg',
     'puesto':'R.R.H.H'
 },
 ]
